8400ac288826f2b934e51aeb07ff52067897e743
ArmandoNCM <armando.castillo@sundevs.com>
2018-08-08 17:02:49 +0000

    Sucessfully implemented automated training script

--- test/test.sh
+++
@@ -1,3 +0,0 @@
-cd steve_carell
-
-curl -d "{'isTraining':true,'name':'SteveCarell'}" -X PUT localhost:3000
---
+++ test/train.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd ./steve_carell
+
+curl -H 'Content-Type: application/json' -X PUT -d '{"isTraining":true,"name":"SteveCarell"}' localhost:3000/openface
+
+curl -X POST -F name="SteveCarell" -F image=@1.jpg localhost:3000/openface
+curl -X POST -F name="SteveCarell" -F image=@2.jpg localhost:3000/openface
+curl -X POST -F name="SteveCarell" -F image=@3.jpg localhost:3000/openface
+curl -X POST -F name="SteveCarell" -F image=@4.jpg localhost:3000/openface
+curl -X POST -F name="SteveCarell" -F image=@5.jpg localhost:3000/openface
+
+sleep 1
+
+curl -H 'Content-Type: application/json' -X PUT -d '{"isTraining":false}' localhost:3000/openface
+
+cd ..
+
+cd ./john_cena
+
+curl -H 'Content-Type: application/json' -X PUT -d '{"isTraining":true,"name":"JohnCena"}' localhost:3000/openface
+
+curl -X POST -F name="JohnCena" -F image=@1.jpg localhost:3000/openface
+curl -X POST -F name="JohnCena" -F image=@2.jpg localhost:3000/openface
+curl -X POST -F name="JohnCena" -F image=@3.jpg localhost:3000/openface
+curl -X POST -F name="JohnCena" -F image=@4.jpg localhost:3000/openface
+curl -X POST -F name="JohnCena" -F image=@5.jpg localhost:3000/openface
+
+sleep 1
+
+curl -H 'Content-Type: application/json' -X PUT -d '{"isTraining":false}' localhost:3000/openface
