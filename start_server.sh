9fcf6f2fb0dc4ff73f79602f7645b8a5b0423cf8
ArmandoNCM <armando.castillo@sundevs.com>
2018-08-08 16:34:35 +0000

    Corrected startup script

--- start_server.sh
+++ start_server.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
+killall python
+killall npm
+killall node
+
 python openface/demos/web/websocket-server.py &
 
 sleep 5
 
 npm run start &
-
-while true; do
-    read -rsn1 input
-    if [ "$input" = "q" ]; then 
-        echo Finishing and Cleaning Up
-	killall python
-	killall npm
-	killall node
-	exit          
-    fi
-done
-
-
 
