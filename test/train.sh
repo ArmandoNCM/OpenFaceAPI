e1c4add1d7ab46ce73c7dfd8e495a93468dc9c94
Armando Castillo <armando.castillo@sundevs.com>
2018-08-10 10:02:21 -0500

    Updated automated training script to match the new port

--- test/train.sh
+++ test/train.sh
@@ -2,31 +2,31 @@
 
 cd ./steve_carell
 
-curl -H 'Content-Type: application/json' -X PUT -d '{"isTraining":true,"name":"SteveCarell"}' localhost:3000/openface
+curl -H 'Content-Type: application/json' -X PUT -d '{"isTraining":true,"name":"SteveCarell"}' localhost:27500/openface
 
-curl -X POST -F name="SteveCarell" -F training=true -F image=@1.jpg localhost:3000/openface
-curl -X POST -F name="SteveCarell" -F training=true -F image=@2.jpg localhost:3000/openface
-curl -X POST -F name="SteveCarell" -F training=true -F image=@3.jpg localhost:3000/openface
-curl -X POST -F name="SteveCarell" -F training=true -F image=@4.jpg localhost:3000/openface
-curl -X POST -F name="SteveCarell" -F training=true -F image=@5.jpg localhost:3000/openface
+curl -X POST -F name="SteveCarell" -F training=true -F image=@1.jpg localhost:27500/openface
+curl -X POST -F name="SteveCarell" -F training=true -F image=@2.jpg localhost:27500/openface
+curl -X POST -F name="SteveCarell" -F training=true -F image=@3.jpg localhost:27500/openface
+curl -X POST -F name="SteveCarell" -F training=true -F image=@4.jpg localhost:27500/openface
+curl -X POST -F name="SteveCarell" -F training=true -F image=@5.jpg localhost:27500/openface
 
 sleep 1
 
-curl -H 'Content-Type: application/json' -X PUT -d '{"isTraining":false}' localhost:3000/openface
+curl -H 'Content-Type: application/json' -X PUT -d '{"isTraining":false}' localhost:27500/openface
 
 cd ..
 
 cd ./john_cena
 
-curl -H 'Content-Type: application/json' -X PUT -d '{"isTraining":true,"name":"JohnCena"}' localhost:3000/openface
+curl -H 'Content-Type: application/json' -X PUT -d '{"isTraining":true,"name":"JohnCena"}' localhost:27500/openface
 
-curl -X POST -F name="JohnCena" -F training=true -F image=@1.jpg localhost:3000/openface
-curl -X POST -F name="JohnCena" -F training=true -F image=@2.jpg localhost:3000/openface
-curl -X POST -F name="JohnCena" -F training=true -F image=@3.jpg localhost:3000/openface
-curl -X POST -F name="JohnCena" -F training=true -F image=@4.jpg localhost:3000/openface
-curl -X POST -F name="JohnCena" -F training=true -F image=@5.jpg localhost:3000/openface
+curl -X POST -F name="JohnCena" -F training=true -F image=@1.jpg localhost:27500/openface
+curl -X POST -F name="JohnCena" -F training=true -F image=@2.jpg localhost:27500/openface
+curl -X POST -F name="JohnCena" -F training=true -F image=@3.jpg localhost:27500/openface
+curl -X POST -F name="JohnCena" -F training=true -F image=@4.jpg localhost:27500/openface
+curl -X POST -F name="JohnCena" -F training=true -F image=@5.jpg localhost:27500/openface
 
 sleep 1
 
-curl -H 'Content-Type: application/json' -X PUT -d '{"isTraining":false}' localhost:3000/openface
+curl -H 'Content-Type: application/json' -X PUT -d '{"isTraining":false}' localhost:27500/openface
 
