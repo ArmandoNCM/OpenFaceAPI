601eaa02bbf5f13b049a113f6790e27a0d248c88
root <root@ip-172-20-0-18.us-west-2.compute.internal>
2018-08-14 03:03:36 +0000

    Reduce wait time and change killall order

--- start_server.sh
+++ start_server.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-killall python
 killall npm
 killall node
+killall python
 
 python websocket-server.py --threshold 0.75 >> logs/python-server.log &
 
-sleep 15
+sleep 5
 
 npm run start >> logs/node-server.log &
 
